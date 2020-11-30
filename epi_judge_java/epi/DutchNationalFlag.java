563cb31eb767beeac9bafb12b9b979516310031b
yunhojang93 <yunhojang93@gmail.com>
2020-11-30 09:45:37 +0900

    2020-11-30 primitives and arrays

--- epi_judge_java/epi/DutchNationalFlag.java
+++ epi_judge_java/epi/DutchNationalFlag.java
@@ -1,22 +1,68 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
 public class DutchNationalFlag {
-  public enum Color { RED, WHITE, BLUE }
+  public enum Color {RED, WHITE, BLUE}
 
   public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
-    // TODO - you fill in here.
-    return;
+    Color pivot = A.get(pivotIndex);
+    int smaller = 0;
+    for (int i = 0; i < A.size(); i++) {
+      if (A.get(i).ordinal() < pivot.ordinal()) {
+        Collections.swap(A, smaller++, i);
+      }
+    }
+    int larger = A.size() - 1;
+    for (int i = A.size() - 1; i >= 0; i--) {
+      if (A.get(i).ordinal() > pivot.ordinal()) {
+        Collections.swap(A, larger--, i);
+      }
+    }
   }
+
+  public static void dutchFlagPartition2(int pivotIndex, List<Color> A) {
+    Color pivot = A.get(pivotIndex);
+    int smaller = 0, equal = 0, larger = A.size();
+    while (equal < larger) {
+      if (A.get(equal).ordinal() < pivot.ordinal()) {
+        Collections.swap(A, smaller++, equal++);
+      } else if (A.get(equal).ordinal() == pivot.ordinal()) {
+        equal++;
+      } else {
+        Collections.swap(A, --larger, equal);
+      }
+    }
+  }
+
+  public static void fourPartitions(List<Integer> A) {
+    int end = fourPartitionsInternal(A, 0, A.size(), 2);
+    fourPartitionsInternal(A, 0, end, 1);
+    fourPartitionsInternal(A, end, A.size(), 3);
+  }
+
+  private static int fourPartitionsInternal(List<Integer> A, int start, int end, int pivot) {
+    int smaller = start;
+    for (int i = start; i < end; i++) {
+      if (A.get(i) < pivot) {
+        Collections.swap(A, smaller++, i);
+      }
+    }
+    return smaller;
+  }
+
   @EpiTest(testDataFile = "dutch_national_flag.tsv")
   public static void dutchFlagPartitionWrapper(TimedExecutor executor,
                                                List<Integer> A, int pivotIdx)
-      throws Exception {
+          throws Exception {
     List<Color> colors = new ArrayList<>();
     int[] count = new int[3];
 
@@ -47,17 +93,19 @@ public class DutchNationalFlag {
 
     if (i != colors.size()) {
       throw new TestFailure("Not partitioned after " + Integer.toString(i) +
-                            "th element");
+              "th element");
     } else if (count[0] != 0 || count[1] != 0 || count[2] != 0) {
       throw new TestFailure("Some elements are missing from original array");
     }
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
+    GenericTest
             .runFromAnnotations(args, "DutchNationalFlag.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+                    new Object() {
+                    }.getClass().getEnclosingClass());
+    List<Integer> A = Arrays.asList(1, 3, 0, 2, 1, 0, 2, 3, 1);
+    fourPartitions(A);
+    System.out.println(A);
   }
 }
--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
+
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    x ^= x >>> 32;
+    x ^= x >>> 16;
+    x ^= x >>> 8;
+    x ^= x >>> 4;
+    x ^= x >>> 2;
+    x ^= x >>> 1;
+    return (short) (x & 0x1);
+  }
+
+  public static short propagateRightmost(short x) {
+    return (short) (x | (x - 1));
+  }
+
+  public static int getMod(int x, int d) {
+    return x & (d - 1);
+  }
+
+  public static boolean isPowerOfTwo(int x) {
+    return x != 0 && (x & (x - 1)) == 0;
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
+    GenericTest
             .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+                    new Object() {
+                    }.getClass().getEnclosingClass());
+    System.out.println(Integer.toBinaryString(propagateRightmost((short) 80))); // 1011111
+    System.out.println(isPowerOfTwo(7)); // false
+    System.out.println(isPowerOfTwo(1024)); // true
+    System.out.println(getMod(77, 64)); // 13
   }
 }
