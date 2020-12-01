ea78efcc9d1ad375948f8928933ec26b0b5dfb1a
yunhojang93 <yunhojang93@gmail.com>
2020-12-01 09:54:46 +0900

    2020-12-01 arrays and strings

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -1,20 +1,52 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
-public class BuyAndSellStock {
-  @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
-  public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BuyAndSellStock.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class BuyAndSellStock {
+    @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
+    public static double computeMaxProfit(List<Double> prices) {
+        // time: O(N) / space: O(N)
+        int size = prices.size();
+        List<Double> mins = new ArrayList<>(size);
+        List<Double> maxs = new ArrayList<>(size);
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < size; i++) {
+            min = Math.min(min, prices.get(i));
+            max = Math.max(max, prices.get(size - i - 1));
+            mins.add(min);
+            maxs.add(max);
+        }
+        double maxProfit = 0;
+        for (int i = 0; i < size; i++) {
+            double profit = maxs.get(size - i - 1) - mins.get(i);
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+
+    @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
+    public static double computeMaxProfit2(List<Double> prices) {
+        // time: O(N) / space: O(N)
+        double min = Double.MAX_VALUE;
+        double maxProfit = 0;
+        for (Double price : prices) {
+            min = Math.min(min, price);
+            maxProfit = Math.max(maxProfit, price - min);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "BuyAndSellStock.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -1,20 +1,61 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
-public class BuyAndSellStockTwice {
-  @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
-  public static double buyAndSellStockTwice(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "BuyAndSellStockTwice.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class BuyAndSellStockTwice {
+    @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
+    public static double buyAndSellStockTwice(List<Double> prices) {
+        // time: O(N), space: O(N)
+        int size = prices.size();
+        if (size <= 1) {
+            return 0;
+        }
+        double min = Integer.MAX_VALUE;
+        double maxProfit = 0;
+        List<Double> maxProfits = new ArrayList<>(size);
+        for (Double price : prices) {
+            min = Math.min(min, price);
+            maxProfit = Math.max(maxProfit, price - min);
+            maxProfits.add(maxProfit);
+        }
+        double totalProfit = 0;
+        double max = Integer.MIN_VALUE;
+        for (int i = size - 1; i >= 0; i--) {
+            max = Math.max(max, prices.get(i));
+            totalProfit = Math.max(totalProfit, maxProfits.get(i) + max - prices.get(i));
+        }
+        return totalProfit;
+    }
+
+    // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/solution/
+    @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
+    public static double buyAndSellStockTwice2(List<Double> prices) {
+        // time: O(N), space: O(1)
+        int size = prices.size();
+        if (size <= 1) {
+            return 0;
+        }
+        double t1Cost = Double.MAX_VALUE, t2Cost = Double.MAX_VALUE;
+        double t1Profit = 0, t2Profit = 0;
+        for (Double price : prices) {
+            t1Cost = Math.min(t1Cost, price);
+            t1Profit = Math.max(t1Profit, price - t1Cost);
+            t2Cost = Math.min(t2Cost, price - t1Profit);
+            t2Profit = Math.max(t2Profit, price - t2Cost);
+        }
+        return t2Profit;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "BuyAndSellStockTwice.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
